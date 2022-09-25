290c8a5dc83c32ade25fdd42a97398c0273f6855
dcanitez <dilaycanitez@gmail.com>
2022-09-25 17:36:34 +0300

    full width button component created

---
+++ myapp/src/components/Button/Button.styled.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Styled = styled.button`
+  width: 100%;
+  color: #fff;
+  background-color: #4071f4;
+  height: 2.5rem;
+  outline: none;
+  border: none;
+  border-radius: 0.5rem;
+  cursor: pointer;
+  font-weight: 600;
+  margin-bottom: 0.5rem;
+  &:hover {
+    background-color: darkblue;
+  }
+  &:active {
+    background-color: #4071f4;
+  }
+`;
