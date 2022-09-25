1e5275b76952c5fc368dd1ed5f7cc2de66c9bc14
dcanitez <dilaycanitez@gmail.com>
2022-09-25 23:37:27 +0300

    change method added

--- myapp/src/pages/ChangePassword/ChangePassword.tsx
+++ myapp/src/pages/ChangePassword/ChangePassword.tsx
@@ -1,14 +1,44 @@
-import React from "react";
+import React, { FC, useState } from "react";
 import { ChangePasswordForm } from "../../components/forms";
+import { ChangePasswordFormProps } from "../../components/forms/ChangePasswordForm/ChangePassword.types";
+import axios from "axios";
+import { ChangePasswordPageProps } from "./ChangePassword.types";
+import { Styled } from "./ChangePassword.styled";
 
-const ChangePassword = () => {
-  const handleChangePassword = () => {
-    console.log("password changed");
+const ChangePassword: FC<ChangePasswordPageProps> = (props) => {
+  const [isChanged, setIsChanged] = useState<boolean>(false);
+  const [showMessage, setShowMessage] = useState<boolean>(false);
+
+  const handleChangePassword: ChangePasswordFormProps["onChangePassword"] = (
+    values
+  ) => {
+    axios
+      .post("http://localhost:80/auth/password-change", values, {
+        headers: {
+          Authorization: `Bearer ${props.token}`,
+        },
+      })
+      .then((res) => {
+        setShowMessage(true);
+        if (res.status === 200) {
+          setIsChanged(true);
+        }
+      })
+      .catch((err) => console.log(err));
   };
   return (
-    <div style={{ marginTop: "10vh" }}>
+    <Styled>
       <ChangePasswordForm onChangePassword={handleChangePassword} />
-    </div>
+      {showMessage ? (
+        <p>
+          <span className={`message ${!isChanged ? "fail" : ""}`}>
+            {!isChanged
+              ? "Something went wrong"
+              : "Password Successfully Changed!"}
+          </span>
+        </p>
+      ) : null}
+    </Styled>
   );
 };
 
