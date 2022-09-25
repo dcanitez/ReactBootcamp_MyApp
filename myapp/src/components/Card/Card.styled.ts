e95e8bf878a74d32daff05a6db267fdb0f0b378c
dcanitez <dilaycanitez@gmail.com>
2022-09-25 17:36:52 +0300

    Card component added

---
+++ myapp/src/components/Card/Card.styled.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Styled = styled.div`
+  background-color: white;
+  width: 400px;
+  border-radius: 5px;
+  box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.4);
+  padding: 40px 32px;
+  box-sizing: border-box;
+  h1 {
+    font-size: 1.2rem;
+    color: #515151;
+    margin: 0 0 40px 0;
+    padding: 0;
+    text-decoration: underline solid #5d7cc7 2px;
+  }
+`;
