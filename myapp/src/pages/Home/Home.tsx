6b3d5d4ebaaf28169c2c20098bb3071a3d863ec1
dcanitez <dilaycanitez@gmail.com>
2022-09-25 19:00:59 +0300

    Home page added

---
+++ myapp/src/pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Styled } from "./Home.styled";
+
+const Home = () => {
+  return (
+    <Styled>
+      <h1>
+        Welcome to Akbank React Bootcamp
+        <br /> Week#5 Practice
+      </h1>
+    </Styled>
+  );
+};
+
+export default Home;
