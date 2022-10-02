6d1286f158e60c664704dd81570b0abd8d148d12
dcanitez <dilaycanitez@gmail.com>
2022-10-02 23:59:32 +0300

    todo page added

--- myapp/src/pages/Todo/Todo.tsx
+++ myapp/src/pages/Todo/Todo.tsx
@@ -1,10 +1,47 @@
 import React from "react";
+import AddCategoryForm from "../../components/forms/AddCategoryForm";
+import AddStatusForm from "../../components/forms/AddStatusForm";
+import AddTodoForm from "../../components/forms/AddTodoForm";
+import { List as CategoryList } from "../../components/lists/Category";
+import { List as StatusList } from "../../components/lists/Status";
 import Tabs from "../../components/Tabs";
+
+const tabs = [
+  {
+    id: "todoList",
+    title: "Todo List",
+    component: (
+      <>
+        <AddTodoForm />
+      </>
+    ),
+  },
+  {
+    id: "categories",
+    title: "Categories",
+    component: (
+      <>
+        <AddCategoryForm />
+        <CategoryList />
+      </>
+    ),
+  },
+  {
+    id: "status",
+    title: "Status",
+    component: (
+      <>
+        <AddStatusForm />
+        <StatusList />
+      </>
+    ),
+  },
+];
 
 const Todo = () => {
   return (
     <div style={{ marginTop: "10vh" }}>
-      <Tabs />
+      <Tabs {...{ tabs }} />
     </div>
   );
 };
